acfe386debc10ebf1836d5a31c0b4e6e8378ee41
ibrahimayaz <ayazibrahim13@gmail.com>
2023-03-08 14:19:26 +0300

    helloworld uygulaması yapıldı

--- Hafta01/01_helloWorld.dart
+++ Hafta01/01_helloWorld.dart
@@ -1,1 +1,9 @@
 //GİRİŞ
+
+// main fonsiyonu ana fonksiyondur. Bütün dart kodları bu ana fonskiyon
+// üzerinden çalışır.
+
+void main() {
+  //Ekrana yazı yazdırmak için print anahtar cümlesi kullanılır.
+  print("Hello World");
+}
